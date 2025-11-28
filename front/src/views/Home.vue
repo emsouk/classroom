22ccedd0d6de5907520e9bd30ba4d60a82c12a8f
Wilfried <wilbou.adrar@gmail.com>
2025-11-28 10:56:09 +0100

    tailwinds

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Connexion</h1>
+        <h1 class="text-4xl font-bold text-red-500">Ceci est rouge</h1>
         <form>
             <input type="email" placeholder="Email" />
             <input type="password" placeholder="Mot de passe" />
