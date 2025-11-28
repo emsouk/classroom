caf900563ce7597681567fd755443619e10e8a19
Wilfried <wilbou.adrar@gmail.com>
2025-11-28 10:29:12 +0100

    router

---
+++ front/src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Connexion</h1>
+        <form>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Mot de passe" />
+            <button>Se connecter</button>
+        </form>
+    </div>
+</template>
